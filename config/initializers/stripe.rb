de8eab8c9cadaa9590206e53363d5b44a5a2a3c4
Amy <amy.pound@icloud.com>
2016-11-09 22:05:08 +0000

    Sold Out Badge Incorporated

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => ENV['pk_test_E0uIkN74j9XfWGdmdmio6HPy'],
+  :secret_key      => ENV['sk_test_iok9wpAzH9qzUhNVc8FIhHOS']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
