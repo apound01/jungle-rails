8ee68f36a255d815573d596653b30ebe4a6d0285
Amy <amy.pound@icloud.com>
2016-11-12 16:22:19 +0000

    Order details page completed

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_E0uIkN74j9XfWGdmdmio6HPy'],
-  :secret_key      => ENV['sk_test_iok9wpAzH9qzUhNVc8FIhHOS']
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+  :secret_key      => ENV['STRIPE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
