4d159a1e284068fb0346ab94249b24dbb5706352
Amy <amy.pound@icloud.com>
2016-11-14 04:49:27 +0000

    Author can delete review, logged in users can create reviews, displayed in reverse order

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,20 @@
 class ReviewsController < ApplicationController
 
+  before_action :authorize
+
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(description: params[:review][:description],
+      rating: params[:review][:rating],
+      user_id: session[:user_id],
+      product_id: params[:product_id])
+
+    @review.user = current_user
+      @review.save
+      redirect_to product_path (params[:product_id])
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 end
