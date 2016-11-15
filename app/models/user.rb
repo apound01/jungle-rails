4b0fec5c5e26bd3b04d98fa6e149a1799a840fbc
Amy <amy.pound@icloud.com>
2016-11-15 16:47:12 +0000

    user tests added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
 
 end
